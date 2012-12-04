d8c5ac248dc22b767b8b2e5d9bb03234f96c0275
Mario Noble <mario@contextmultimedia.com>
2012-12-03 22:32:09 -0800

    css nav fixes from zerosix

--- js/nav-toggler.js
+++ js/nav-toggler.js
@@ -1,5 +1,6 @@
 
-var myDeviceWidth = $().width();
+
+var myDeviceWidth = $(window).width();
 
 if (myDeviceWidth <= 600) {
 
@@ -20,37 +21,11 @@ $('.nav-toggler').toggle(function() {
 	}
 );
 
+
+
 } else {
 
 	// do nothing
 
 }
-
-//for testing
-
-$(window).resize(function() {
-
-	var myDeviceWidth = $(window).width();
-
-	if (myDeviceWidth <= 600) {
-
-$('#header-nav ul li').first().prepend("<li><a href='#' class='nav-toggler'>+ Open Navigation </a></li>");
-$('#header-nav ul li a').css("display","none");
-$('#header-nav ul li a.nav-toggler').css("display","block");
-
-$('.nav-toggler').toggle(function() {
-	$('#header-nav ul li a').css("display","block");
-	$('#header-nav ul li a').not('.nav-toggler').show("slow");
-	$(this).html("- Close Navigation");
-	
-}, function() {
-	$('#header-nav ul li a').css("display","block");
-	$('#header-nav ul li a').not('.nav-toggler').hide("slow");
-	$(this).html("+ Open Navigation");
-
-	}
-);
-
-}
-
 
